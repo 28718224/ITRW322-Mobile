d75e7f4fe18e99f64558a7e92b24c48efbc4b332
ChristiaanKoch98 <28718224@student.g.nwu.ac.za>
2019-08-26 21:33:06 +0200

    Added profile page module code

--- src/pages/profile/profile.module.ts
+++ src/pages/profile/profile.module.ts
@@ -1,16 +1,42 @@
-import { NgModule } from '@angular/core';
-import { IonicPageModule } from 'ionic-angular';
-import { ProfilePage } from './profile';
-
-@NgModule({
-  declarations: [
-    ProfilePage,
-  ],
-  imports: [
-    IonicPageModule.forChild(ProfilePage),
-  ],
-  exports: [
-    ProfilePage
-  ]
+import { Component, NgZone } from '@angular/core';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
+import { ImghandlerProvider } from '../../providers/imghandler/imghandler';
+import { UserProvider } from '../../providers/user/user';
+import firebase from 'firebase';
+/**
+ * Generated class for the ProfilePage page.
+ *
+ * See http://ionicframework.com/docs/components/#navigation for more info
+ * on Ionic pages and navigation.
+ */
+@IonicPage()
+@Component({
+  selector: 'page-profile',
+  templateUrl: 'profile.html',
 })
-export class ProfilePageModule {}
+export class ProfilePage {
+  avatar: string;
+  displayName: string;
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+    public userservice: UserProvider, public zone: NgZone, public alertCtrl: AlertController,
+    public imghandler: ImghandlerProvider) {
+  }
+ 
+  ionViewWillEnter() {
+    this.loaduserdetails();
+  }
+ 
+  loaduserdetails() {
+    this.userservice.getuserdetails().then((res: any) => {
+      this.displayName = res.displayName;
+      this.zone.run(() => {
+        this.avatar = res.photoURL;
+      })
+    })
+  }
+  logout() {
+    firebase.auth().signOut().then(() => {
+      this.navCtrl.parent.parent.setRoot('LoginPage');
+    })
+  }
+}
