f278c40cb3d442e5a4145005cc47f585cada5fc7
Christiaankoch98 <28718224@student.g.nwu.ac.za>
2019-09-20 21:54:09 +0200

    Groups should now be fully working

--- src/pages/groups/groups.ts
+++ src/pages/groups/groups.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, Events, LoadingController } from 'ionic-angular';
-//import { GroupsProvider } from '../../providers/groups/groups';
+import { GroupsProvider } from '../../providers/groups/groups';
 /**
  * Generated class for the GroupsPage page.
  *
@@ -15,11 +15,32 @@ import { IonicPage, NavController, NavParams, Events, LoadingController } from '
 export class GroupsPage {
   allmygroups;
   constructor(public navCtrl: NavController, public navParams: NavParams, public events: Events,
-              public loadingCtrl: LoadingController,/* public groupservice: GroupsProvider*/) {
+              public loadingCtrl: LoadingController, public groupservice: GroupsProvider) {
+  }
+
+  ionViewWillEnter() {
+    let loader = this.loadingCtrl.create({
+      content: 'Getting your groups, Please wait...'
+    });
+    loader.present();
+    this.groupservice.getmygroups();
+    loader.dismiss();
+    this.events.subscribe('allmygroups', () => {
+      this.allmygroups = this.groupservice.mygroups;
+    })
+  }
+
+  ionViewDidLeave() {
+    this.events.unsubscribe('allmygroups');
   }
 
   addgroup() {
     this.navCtrl.push('NewgroupPage');
+  }
+
+  openchat(group) {
+    this.groupservice.getintogroup(group.groupName);
+    this.navCtrl.push('GroupchatPage', { groupName: group.groupName });
   }
 
 }
